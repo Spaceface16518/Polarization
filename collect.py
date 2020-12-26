5a0cf28207ae71f777111faab5656cbcc2f85b77
Spaceface16518 <amrit.rathie@gmail.com>
2020-12-26 17:07:21 -0600

    rework collect.py
    saving must happen manually. the collect function is now labelled `fetch` to reflect its behavior.

--- collect.py
+++ collect.py
@@ -7,16 +7,20 @@ import pandas
 from settings import reddit
 
 
-def collect(subreddit_name="politics", limit=1000, prefer_cache=True, cache=False):
-    if prefer_cache:
-        # TODO: return fresh results if file DNE
-        return pandas.read_csv('data/comments.csv')
-    else:
-        df = pandas.DataFrame(reddit.subreddit(subreddit_name).comments(limit=limit))
-        if cache:
-            df.to_csv('data/comments.csv')
-        return df
+def comment_dict(comment, attributes=None):
+    if attributes is None:
+        # default value
+        attributes = ['author', 'body', 'created_utc', 'distinguished', 'edited', 'id', 'is_submitter', 'link_id',
+                      'parent_id', 'permalink', 'score', 'stickied', 'subreddit_id']
+    result = {}
+    for attribute in attributes:
+        result[attribute] = getattr(comment, attribute)
+    return result
+
+
+def fetch(subreddit_name="politics", limit=100):
+    return pandas.DataFrame(map(comment_dict, reddit.subreddit(subreddit_name).comments(limit=limit)))
 
 
 if __name__ == '__main__':
-    collect(prefer_cache=False, cache=True)
+    fetch().to_csv('data/comments.csv')
